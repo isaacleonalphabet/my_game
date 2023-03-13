36ba56e1b4a5d759e50090529b64e742987834d4
isaacleonalphabet <isaac.leon23@bcp.org>
2023-03-13 10:26:19 -0700

    test

---
+++ settings.py
@@ -0,0 +1,20 @@
+# File created by: 
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+MOB_ACC = .12
+MOB_FRICTION = 0.12
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50, 50, 255)
+RED = (255, 50, 50)
