71a0b43c18a311b236187956c14dcbc304014fea
isaacleonalphabet <126900992+isaacleonalphabet@users.noreply.github.com>
2023-03-30 13:57:48 -0700

    test

--- settings.py
+++ settings.py
@@ -16,5 +16,6 @@ MOB_FRICTION = 0.12
 
 # colors
 BLACK = (0,0,0)
-BLUE = (50, 50, 255)
+BLUE = (255, 255, 255)
 RED = (255, 50, 50)
+WHITE = (255,255,255)
--- sprites.py
+++ sprites.py
@@ -1,19 +1,24 @@
-# File created by Isaac Leon
+# File created by: Isaac Leon 
+
 import pygame as pg
 
 from pygame.sprite import Sprite
 
 from settings import *
 
+from random import randint
+
 vec = pg.math.Vector2
 
 
-# create a player 
+# create a player
 
 class Player(Sprite):
-    def __init__(self):
+    def __init__(self, game):
         Sprite.__init__(self)
+        self.game = game
         self.image = pg.Surface((50,50))
+        # self.image = pg.transform.scale((50, 38))
         self.image.fill(BLACK)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
@@ -23,20 +28,30 @@ class Player(Sprite):
         self.canjump = False
     def input(self):
         keystate = pg.key.get_pressed()
-        if keystate[pg.K_w]:
-            self.acc.y = -PLAYER_ACC
+        # if keystate[pg.K_w]:
+        #     self.acc.y = -PLAYER_ACC
         if keystate[pg.K_a]:
             self.acc.x = -PLAYER_ACC
-        if keystate[pg.K_s]:
-            self.acc.y = PLAYER_ACC
+        # if keystate[pg.K_s]:
+        #     self.acc.y = PLAYER_ACC
         if keystate[pg.K_d]:
             self.acc.x = PLAYER_ACC
+    def jump(self):
+        # jump only if standing on a platform
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        if hits:
+            self.vel.y = -PLAYER_JUMP
     def update(self):
-        self.acc = self.vel * PLAYER_FRICTION
+        self.acc = vec(0, PLAYER_GRAV)
+        
+        self.acc.x = self.vel.x * PLAYER_FRICTION
         self.input()
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
-        self.rect.center = self.pos
+        self.rect.midbottom = self.pos
+        # test to see if off screen
         if self.rect.x > WIDTH:
             print("I'm off the right screen...")
         if self.rect.x < 0:
@@ -53,33 +68,29 @@ class Mob(Sprite):
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0,0)
+        self.vel = vec(randint(1,5),randint(1,5))
         self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
     def behavior(self):
-        self.acc.y = -MOB_ACC
-        # self.acc.x = -MOB_ACC 
-        # self.acc.y = MOB_ACC
-        # self.acc.x = MOB_ACC
-        if self.rect.x > WIDTH:
-            print("I'm off the right screen...")
-        if self.rect.x < 0:
-            print("I'm off the left screen...")
-        if self.rect.y < 0:
-            print("I'm off the top screen...")
-            # This reduces velocity but its still going to go up 
+        if self.rect.x > WIDTH or self.rect.x < 0 or self.rect.y > HEIGHT or self.rect.y < 0:
             self.vel *= -1
-            self.pos.y = HEIGHT 
-        if self.rect.y > HEIGHT:
-            print("I'm off the bottom screen...")
+
     def update(self):
-        self.acc = self.vel * MOB_FRICTION
         self.behavior()
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
+        self.pos += self.vel
         self.rect.center = self.pos
 
+class Platform(Sprite):
+    def __init__(self, x, y, w, h):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w,h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+ #if the cube touches the side of the game box. then the player stops
+       
 
-    
-    
+        
+        
