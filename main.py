cb58d449c6f84f4e66ee8073e8709280e1438056
isaacleonalphabet <126900992+isaacleonalphabet@users.noreply.github.com>
2023-03-30 14:31:38 -0700

    test

--- main.py
+++ main.py
@@ -1,46 +1,75 @@
-#File created by: Isaac Leon
+# Agenda:
+# gIT GITHUB    
+# Build file and folder structures
+# Create libraries
+# testing github changes
+# I changed something - I changed something else tooooo!
+
+# This file was created by: Chris Cozort
+# Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
+# Sources: 
+
+# import libs
 import pygame as pg
-import random
 import os
-
-# need this to create file paths effectively
-from os import path
-
-# import settings
+# import settings 
 from settings import *
 from sprites import *
-from random import randint
 # from pg.sprite import Sprite
 
-# set up assets folders
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, "images")
+'''
+My goal for the research project is: 
 
-#Creates the game class to organize game content 
+Jumping over blocks and move around 
+Make another block collide with me and have the temrinal say "you collided with xyz"
+
+
+Reach Goal: 
+
+make things shoot
+'''
+
+# set up assets folders
+# This just installs the images 
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+# create game class in order to pass properties to the sprites file
+# This is a class (a gorup of descriptions for a player, enemy, or object)
 class Game:
-    #inits pygame thingfs including setting i[ screen/display area 
     def __init__(self):
+        # init game window etc.
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption("My Game...")
+        pg.display.set_caption("my game")
         self.clock = pg.time.Clock()
         self.running = True
-#This is a method to create a new game 
-
+        print(self.screen)
     def new(self):
-            self.score = 0
-            self.all_sprites = pg.sprite.Group()
-            self.platforms = pg.sprite.Group()
-            self.enemies = pg.sprite.Group()
-            # instnatiates player class from sprites file, and passes this ham class as 
-            # an argument 
-            self.player = Player(self)
-            self.all_sprites.add(self.player)
-            for i in range(1,10):
-                e = Mob()
-                self.all_sprites.add(e)
-            self.run()
+        # starting a new game
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+        # This says how high the player can jump and how wide it can go 
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        self.all_sprites.add(self.plat1)
+
+        self.platforms.add(self.plat1)
+        
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+         p = Platform(*plat)
+        self.all_sprites.add(p)
+        self.platforms.add(p)
+        for i in range(0,10):
+            m = Mob(20,20,(0,255,0))
+            self.all_sprites.add(m)
+            self.enemies.add(m)
+        self.run()
     def run(self):
         self.playing = True
         while self.playing:
@@ -48,10 +77,9 @@ class Game:
             self.events()
             self.update()
             self.draw()
+    
     def events(self):
-        # Game Loop - events
         for event in pg.event.get():
-            # check for closing window
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
@@ -59,30 +87,46 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.player.jump()
+
+    # This describes thre collision poutn of the player to the enemies 
+    def update(self):
+        self.all_sprites.update()
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                # When I hit the object, it iwll magically dissapear 
+                if hits[0].variant == "disappearing":
+                    hits[0].kill()
+                    # The enemy will be bouncing aorund the screen and not run away fromt he view box when I run it 
+                elif hits[0].variant == "bouncey":
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = -PLAYER_JUMP
+                else:
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = 0
+
+    def draw(self):
+        self.screen.fill(BLUE)
+        self.all_sprites.draw(self.screen)
+        # this is a function
+        pg.display.flip()
+        # The descrioption show how big the screen will be 
     def draw_text(self, text, size, color, x, y):
         font_name = pg.font.match_font('arial')
         font = pg.font.Font(font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
+        # The little green striaght lines to stand on 
+        text_rect.midtop = (x,y)
         self.screen.blit(text_surface, text_rect)
-    # def get_mouse_now():
-    #     x,y = pg.mouse.get_pos()
-    #     return (x,y)
-    def update(self):
-        self.all_sprites.update()
-    def draw(self):
-        self.screen.fill(BLUE)
-        self.draw_text("Hello there!", 42, WHITE, WIDTH/2, HEIGHT/10)
-        self.all_sprites.draw(self.screen)
-        pg.display.flip()
+    def get_mouse_now(self):
+        x,y = pg.mouse.get_pos()
+        return (x,y)
 
-vec = pg.math.Vector2
-
-# game loop
-
+# instantiate the game class...
 g = Game()
 
+# kick off the game loop
 while g.running:
     g.new()
 
