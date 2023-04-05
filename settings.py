87853509620b04fd7ed2f0e1948c129242f24d1b
isaacleonalphabet <126900992+isaacleonalphabet@users.noreply.github.com>
2023-04-04 22:05:40 -0700

    test

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 WIDTH = 800
 HEIGHT = 600
 PLAYER_ACC = 2
-PLAYER_FRICTION = -0.3
-PLAYER_JUMP = 20
-PLAYER_GRAV = 0.8
+PLAYER_FRICTION = -0.5
+PLAYER_JUMP = 10
+PLAYER_GRAV = -0.5
 MOB_ACC = 2
 MOB_FRICTION = -0.3
 BLACK = (0,0,0)
@@ -16,8 +16,10 @@ SCORE = 0
 PAUSED = False
 
 # Starting platforms
+# I am starting a new platform on the other side of the game 
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (100,255,100), "bouncey"),
-                 (125, HEIGHT - 350, 100, 5, (200,100,50), "disappearing"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (200,200,200), "bouncey"),
+                 (125, HEIGHT - 350, 100, 5, (200,200,200), "disappearing "),
                  (350, 200, 100, 20, (200,200,200), "normal"),
-                 (175, 100, 50, 20, (200,200,200), "normal")]
+                 (175, 100, 50, 20, (200,200,200), "normal")
+                 (500,200, 80, 300, (200,20,200, "normal"))]
